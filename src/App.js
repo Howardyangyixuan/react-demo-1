4f85b0b727c3b1f407101e59420e09153fdca9a3
howardyangyixuan <howardyangyixuan@gmail.com>
2021-01-26 07:33:03 +0800

    上一个提交是复杂的表单，这里用简单的，来实现useState的功能

--- src/App.js
+++ src/App.js
@@ -2,50 +2,33 @@ import React, {useReducer, useRef} from "react"
 import "./App.css"
 
 //1.初始化
-const initFormData = {
-  name: "",
-  age: 18,
-  ethic: "汉族"
+const initData = {
+  n:0
 }
 
 //2.提供操作
-function dispatch(state, action) {
-  if (action.type === "update") {
-    return {...state, ...action.formData}
-  } else if (action.type === "reset") {
-    return initFormData
+function reducer(state, action) {
+  if (action.type === "add"){
+    return {n:state.n+1}
+  } else if (action.type === "double") {
+    console.log("2")
+    console.log(state)
+    return {n:state.n*2}
   } else {
-    console.log(action)
     throw new Error("unknown type")
   }
 }
 
 function App() {
-  const [formData, setFormData] = useReducer(dispatch, initFormData)
-  const reset = () => {
-    setFormData({type:"reset"})
-  }
+  const [n,setN] = useReducer(reducer,initData)
   return (
-    <form onSubmit={reset} onReset={reset}>
-      <div>
-        <label>name
-          <input value={formData.name} onChange={e => setFormData({type: "update", formData: {name: e.target.value}})}/></label>
-      </div>
-      <div>
-        <label>age
-          <input value={formData.age} onChange={e => setFormData({type: "update", formData: {age: e.target.value}})}/></label>
-      </div>
-      <div>
-        <label>name
-          <input value={formData.ethic} onChange={e => setFormData({type: "update", formData: {ethic: e.target.value}})}/></label>
-      </div>
-      <div>
-        <button type="submit">submit</button>
-        <button type="reset">reset</button>
-      </div>
-      <hr/>
-      {JSON.stringify(formData)}
-    </form>
+    <div>
+      <p>
+        n:{n.n}
+      </p>
+      <button onClick={()=>{setN({type:"double"})}}>*2</button>
+      <button onClick={()=>{setN({type:"add"})}}>+1</button>
+    </div>
 
   )
 }
