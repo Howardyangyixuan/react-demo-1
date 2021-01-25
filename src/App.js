41239842720874af1cb6875b5c5a0ba48f250b6c
howardyangyixuan <howardyangyixuan@gmail.com>
2021-01-25 22:26:37 +0800

    useReducer，即复杂的封装操作的useState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React, {useState, useRef} from "react"
-import ReactDOM from "react-dom"
+import React, {useReducer, useRef} from "react"
 import "./App.css"
 
-const rootElement = document.getElementById("root")
-const themeContext = React.createContext(null)
+//1.初始化
+const initFormData = {
+  name: "",
+  age: 18,
+  ethic: "汉族"
+}
 
+//2.提供操作
+function dispatch(state, action) {
+  if (action.type === "update") {
+    return {...state, ...action.formData}
+  } else if (action.type === "reset") {
+    return initFormData
+  } else {
+    console.log(action)
+    throw new Error("unknown type")
+  }
+}
 
 function App() {
-  const [user, setUser] = React.useState(0)//注意返回对象要外面套一层括号，这是js的bug，函数的方式只执行一次
+  const [formData, setFormData] = useReducer(dispatch, initFormData)
+  const reset = () => {
+    setFormData({type:"reset"})
+  }
   return (
-    <div>
-      <p>user: </p>
-      <p>{user}</p>
-      <button onClick={() => {
-        // setUser(user+1)
-        // setUser(user+1)
-        setUser(user=>user+1)
-        setUser(user=>user+1)
-      }}>
-        grow
-      </button>
-    </div>
+    <form onSubmit={reset} onReset={reset}>
+      <div>
+        <label>name
+          <input value={formData.name} onChange={e => setFormData({type: "update", formData: {name: e.target.value}})}/></label>
+      </div>
+      <div>
+        <label>age
+          <input value={formData.age} onChange={e => setFormData({type: "update", formData: {age: e.target.value}})}/></label>
+      </div>
+      <div>
+        <label>name
+          <input value={formData.ethic} onChange={e => setFormData({type: "update", formData: {ethic: e.target.value}})}/></label>
+      </div>
+      <div>
+        <button type="submit">submit</button>
+        <button type="reset">reset</button>
+      </div>
+      <hr/>
+      {JSON.stringify(formData)}
+    </form>
+
   )
 }
 
