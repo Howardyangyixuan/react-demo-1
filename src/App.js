e1bbf47de5ca37de63b744d69d968a714c5fbea1
howardyangyixuan <howardyangyixuan@gmail.com>
2021-01-29 11:47:44 +0800

    使用useRef并设置useState进行自动更新

--- src/App.js
+++ src/App.js
@@ -1,63 +1,19 @@
-import React, {memo, useCallback, useMemo, useState} from "react"
+import React, {memo, useRef, useState} from "react"
 import "./App.css"
 
 function App() {
-  const [n, setN] = useState(0)
-  const [m, setM] = useState(0)
-  const [r, setR] = useState(0)
+  const autoRender = useState(0)[1]
+  const n = useRef(0)
   const onClick = () => {
-    setN(n + 1)
+    n.current += 1
+    console.log(n)
+    autoRender(n.current)
   }
-  const onClickMemo = useCallback(
-    () => {
-      setN(n + 1)
-    }
-    , [n])
-  //如果deps里不写n那么效果将是静态的，即n始终为1
-  const onClickMemoNoDeps = useMemo(() => {
-    return () => {
-      setN(n + 1)
-    }
-  }, [])
-
 
   return (
     <div>
-      n:{n}
-      <button onClick={() => {
-        setN(n + 1)
-      }}>n+1
-      </button>
-      m:{m}
-      <button onClick={() => {
-        setM(m + 1)
-      }}>m+1
-      </button>
-      r:{r}
-      <button onClick={() => {
-        setR(r + 1)
-      }}>r+1
-      </button>
-      <hr/>
-      memoChild:
-      <MemoChild name={"1.memoChild"} data={m} onClick={onClick}/>
-      useMemoNoDepsChild:
-      <MemoChild name={"2.useMemoNoDepsChild"} data={m} onClick={onClickMemoNoDeps}/>
-      useMemoChild:
-      <MemoChild name={"3.useMemoChild"} data={m} onClick={onClickMemo}/>
-    </div>
-  )
-}
-
-const MemoChild = memo(Child)
-
-function Child(props) {
-  console.log(`${props.name} render`)
-  return (
-    <div>
-      m:{props.data}
-      <button onClick={props.onClick}>
-        n+1
+      n:{n.current}
+      <button onClick={onClick}>n+1
       </button>
     </div>
   )
