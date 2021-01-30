82cc3ab750b981a6e982baa9450c017be0599c21
howardyangyixuan <howardyangyixuan@gmail.com>
2021-01-30 14:43:26 +0800

    传递ref，每次需要forward包装，即可向内部传递，即所谓的forward，还没有完全搞定

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, {createRef, forwardRef, useRef, useState} from "react"
 import "./App.css"
 
 function App() {
+  const buttonRef = useRef(null)
+  const divRef = useRef(null)
   return (
     <div>
-      <MovableButton text="按住可以移动的按钮"/>
-      <MovableDiv text="按住可以移动Div"/>
+      <MovableButton ref={buttonRef} text="按住可以移动的按钮"/>
+      <MovableDiv ref={divRef} text="按住可以移动Div"/>
     </div>
   )
 }
@@ -14,29 +16,30 @@ function App() {
 // const MovableButton = (props) => Movable(props, (props) => <button {...props}/>)
 // const MovableDiv = (props) => Movable(props, (props) => <div {...props}/>)
 //进行如下修改
-const MovableButton = Movable((props) => <button {...props}/>)
-const MovableDiv = Movable((props) => <div {...props}/>)
+const MovableButton = Movable(forwardRef((props, ref) => <button ref={ref} {...props}/>))
+const MovableDiv = Movable(forwardRef((props, ref) => <div ref={ref} {...props}/>))
 
 function Movable(Component) {
-  function ComponentWithProps(props) {
-    //记录移动时鼠标的位置，根据当前位置 = 原位置+当前鼠标位置-上一时刻鼠标位置
+  function ComponentWithProps(props, ref) {
+    //记录移动时DOM对象左上角的位置，根据当前位置 = 原位置+当前鼠标位置-上一时刻鼠标位置
     const [pos, setPos] = useState([0, 0])
     //用useRef在组件中共享变量，而不是重新生成，替代按下press和相对位置dist，同时由于current不自动更新，因此需要改变pos从而使其更新，所以变量的含义发生变化
     //记录点击时，鼠标的位置，保证按钮不跳动
-    const ref = useRef(null)
+    const mousePos = useRef(null)
     const onMouseDown = (e) => {
       console.log("down")
-      console.log(ref.current)
-      ref.current = [e.clientX, e.clientY]
+      console.log(props, ref);
+      console.log(mousePos.current)
+      mousePos.current = [e.clientX, e.clientY]
     }
     const onMouseMove = (e) => {
-      if (ref.current) {
-        setPos([pos[0] + e.clientX - ref.current[0], pos[1] + e.clientY - ref.current[1]])
-        ref.current = [e.clientX, e.clientY]
+      if (mousePos.current) {
+        setPos([pos[0] + e.clientX - mousePos.current[0], pos[1] + e.clientY - mousePos.current[1]])
+        mousePos.current = [e.clientX, e.clientY]
       }
     }
     const onMouseUp = () => {
-      ref.current = null
+      mousePos.current = null
       console.log("up")
     }
 
@@ -46,11 +49,11 @@ function Movable(Component) {
                    position: "fixed",
                    left: pos && pos[0],
                    top: pos && pos[1]
-                 }}>{props.text}</Component>
+                 }} ref={ref}>{props.text}</Component>
     )
   }
 
-  return ComponentWithProps
+  return forwardRef(ComponentWithProps)
 }
 
 
