59e6d389caa158694218dbaa56a7649c7fd41095
howardyangyixuan <howardyangyixuan@gmail.com>
2021-01-30 20:25:16 +0800

    使用自定义Hook完成的在线版好友列表网站

--- src/App.js
+++ src/App.js
@@ -4,16 +4,42 @@ import useList from "./hooks/useList"
 
 function App() {
   console.log("App加载了")
-  const {user, setUser} = useList()
+  const {user, remove, add} = useList()
+  const len = user ? user.length : 0
+  let data = {id: len, name: "null"}
+  const getName = () => {
+    data.name = document.getElementsByTagName("input")[0].value
+    data.id = len + user.length
+    add(data)
+  }
   return (
     <div>
       <h1>我的好友列表</h1>
       <hr/>
       {
         user ? (
-          <ol>
-            {user.map(item => <li key={item.id}>{item.name}</li>)}
-          </ol>
+          <div>
+            {user.length ? (
+            <div>
+              <ol>
+                {user.map((item, index) => {
+                    return (
+                      <li key={item.id}>
+                        {item.name}
+                        <button onClick={() => {
+                          remove(index)
+                        }}>x
+                        </button>
+                      </li>)
+                  }
+                )}
+              </ol>
+            </div>
+            ): <div>您没有朋友</div>
+            }
+            <form action=""><input type="text" name="add"/></form>
+            <button onClick={getName}>add</button>
+          </div>
         ) : ("加载中...")
       }
     </div>
--- src/hooks/useList.js
+++ src/hooks/useList.js
@@ -1,8 +1,28 @@
 import {useEffect, useState} from "react"
 
+let database =
+ [
+  {id: 1, name: "Howard"},
+  {id: 2, name: "Kevin"},
+  {id: 3, name: "Evan"},
+  {id: 4, name: "Michael"},
+  {id: 5, name: "Bill"}
+]
+
 function useList() {
   console.log("useList执行了")
   const [user, setUser] = useState(null)
+  const remove = (index)=>{
+    database.splice(index,1)
+    setUser([...database])
+    console.log(user)
+  }
+
+  const add = (item)=>{
+    database.push(item)
+    setUser([...database])
+    console.log(user)
+  }
   useEffect(
     () => {
       //注意由于每次返回的是一个新的数组，所以user会变化，useState会引发重新渲染执行，因此需要使用useEffect才能避免重复请求
@@ -12,24 +32,19 @@ function useList() {
   return (
     {
       user,
-      setUser
+      remove,
+      add
     }
   )
 }
 
 function ajax(url) {
+  const data = database
   console.log(url)
-  const data = [
-    {id: 1, name: "Howard"},
-    {id: 2, name: "Kevin"},
-    {id: 3, name: "Evan"},
-    {id: 4, name: "Michael"},
-    {id: 5, name: "Bill"}
-  ]
   return new Promise((resolve, reject) => {
     setTimeout(
       () => resolve.call(null, data)
-      , 3000)
+      , 0)
   })
 }
 
